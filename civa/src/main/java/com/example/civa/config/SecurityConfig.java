4f42da5fc6bd4424cc48ba02bdf0fffd56e050b5
Aldair <aldairbroncanosoltelo@gmail.com>
2025-09-21 20:40:25 -0500

    chore(backend): actualizar configuración de seguridad y documentación Swagger

--- civa/src/main/java/com/example/civa/config/SecurityConfig.java
+++ civa/src/main/java/com/example/civa/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.civa.security;
+package com.example.civa.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- civa/src/main/java/com/example/civa/config/SwaggerConfig.java
+++ civa/src/main/java/com/example/civa/config/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.example.civa.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Buses API")
+                        .description("Servicio para extraer información de buses")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Equipo de Desarrollo Civa")
+                                .email("soporte@civa.com")
+                                .url("https://civa.com")));
+    }
+
 }
